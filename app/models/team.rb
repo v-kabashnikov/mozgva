c04625f765c7e3bdaa28ebcb414fc8a6c1869011
K. Kanyuka <kkan.web@gmail.com>
2016-07-14 20:28:59 +0300

    team, member models

---
+++ app/models/team.rb
@@ -0,0 +1,31 @@
+class Team < ApplicationRecord
+  MAX_MEMBERS_COUNT = 9
+  belongs_to :league, optional: true
+  has_many :members, dependent: :destroy
+  has_many :users, through: :members
+
+  before_create :set_invite
+
+  validates :name, presence: true, uniqueness: true
+
+  def generate_invite
+    values = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
+    (1..5).map{ values.sample.to_s }.join
+  end
+
+  def members_count
+    members.count
+  end
+
+  def add_member user
+    Member.create(user: user, team: self)
+  end
+
+  private
+  def set_invite
+    self.invite = loop do
+      inv = generate_invite
+      break inv unless Team.find_by_invite inv
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711173834) do
+ActiveRecord::Schema.define(version: 20160714160654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,34 @@ ActiveRecord::Schema.define(version: 20160711173834) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_leagues_on_name", unique: true, using: :btree
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_members_on_team_id", using: :btree
+    t.index ["user_id", "team_id"], name: "index_members_on_user_id_and_team_id", unique: true, using: :btree
+    t.index ["user_id"], name: "index_members_on_user_id", using: :btree
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "league_id"
+    t.string   "invite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invite"], name: "index_teams_on_invite", unique: true, using: :btree
+    t.index ["league_id"], name: "index_teams_on_league_id", using: :btree
+    t.index ["name"], name: "index_teams_on_name", unique: true, using: :btree
   end
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'role' for column 'role'
 
+  add_foreign_key "members", "teams"
+  add_foreign_key "members", "users"
+  add_foreign_key "teams", "leagues"
 end
