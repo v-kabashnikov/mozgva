f160da331333c41be49b4c5ac6a0b6424aed426c
Andrey Fedorovich <a.d.fedorovich@gmail.com>
2016-07-22 18:10:15 +0300

    add franchisePage

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
   get '/team', to: 'teams#my_team', as: :my_team
   get '/calendar', to: 'home#calendar', as: :calendar
+  get '/franchise', to: 'home#franchise', as: :franchise
   devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
   post '/users/search', to: 'users#search', as: :users_search
   put '/users/update_city', to: 'users#update_city', as: :update_city
