151de4a0121866c6b38c6b75bfa3b119233501b0
K. Kanyuka <kkan.web@gmail.com>
2016-08-01 12:29:57 +0300

    authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.new_user_session_path, alert: exception.message
+  end
+
   private
   def set_city
     @city = if current_user
