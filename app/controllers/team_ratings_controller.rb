46f775470d5d32b8f06200e156f2b7bb7b7a49ed
reiro <reiro123@gmail.com>
2016-08-23 13:51:30 +0300

    add sorting by percent of games

--- app/controllers/team_ratings_controller.rb
+++ app/controllers/team_ratings_controller.rb
@@ -1,12 +1,8 @@
 class TeamRatingsController < ApplicationController
 
 	def rating
-		t = Time.now
 		result = Game.calculate_all_team_ratings
-		g = (Time.now - t).to_s
 		@ratings = Team.pick_up_team_ratings(result)
-		p (Time.now - t).to_s
-		p g
 	end
 
 end
