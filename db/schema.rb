2cae08802cfb33f02e85bdb400e0a2571a74fb5b
K. Kanyuka <kkan.web@gmail.com>
2016-09-12 17:07:07 +0300

    added about field to AchievmentType

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912102909) do
+ActiveRecord::Schema.define(version: 20160912140418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160912102909) do
     t.datetime "image_updated_at"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.text     "about"
   end
 
   create_table "achievments", force: :cascade do |t|
