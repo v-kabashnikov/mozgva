0e789c67b56ac6604a25ad8598c799c6d1d33a8c
K. Kanyuka <kkan.web@gmail.com>
2016-09-13 17:21:56 +0300

    teams for rating by raw sql

--- app/controllers/team_ratings_controller.rb
+++ app/controllers/team_ratings_controller.rb
@@ -1,7 +1,8 @@
 class TeamRatingsController < ApplicationController
 	def rating
 		result = Game.calculate_all_team_ratings
-		@top, @pretendents = Team.pick_up_team_ratings(result)
+		# @top, @pretendents = Team.pick_up_team_ratings(result)
+		@top, @pretendents = Team.sql_pick_up_team_ratings
 		@top.sort! {|a, q| q[:percent] <=> a[:percent]}
 		@pretendents.sort! {|a, q| q[:percent] <=> a[:percent]}
 	end
