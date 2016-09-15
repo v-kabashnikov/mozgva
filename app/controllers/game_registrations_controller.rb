c01d18ecc4adf9ecd9dd0dcb4955342118625f38
shrewhools <vladyukha@yandex.ru>
2016-09-16 01:26:00 +0300

    fix 105 game_registrations

--- app/controllers/game_registrations_controller.rb
+++ app/controllers/game_registrations_controller.rb
@@ -4,16 +4,20 @@ class GameRegistrationsController < ApplicationController
 		@game = Game.find(params[:game_id])
 		team = current_user.team
 		if team
+			if team.members.count >= 3
 			@gr = GameRegistration.new(game: @game, team: team)
 			phone = Phone.new(team.captain.phone)
 			if phone.valid?
-				number = team.members_count   
+				number = team.members_count
 				msg = "Команда \"#{team.name}\" (#{number} #{Russian.p(number, "человек", "человека", "человек")}) зарегистрирована на Мозгву #{@game.when.strftime('%d.%m.%y')}. Ждем Вас!"
 				sms = SmsTwilio.new.send(phone.formatted_phone, msg)
 			end
 			unless @gr.save
 				@gr = nil
 				flash[:modal_errors] = @gr.errors.messages
+			end
+			else
+				flash[:modal_errors] = ["В команде должно быть минимум 3 человека"]
 			end
 		else
 			flash[:modal_errors] = ["Необходимо сперва вступить в команду или создать свою"]
